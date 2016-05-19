40b163b71849e06dfbf8be8945c783a1c788d39d
#云淡然 <cloudcome@163.com>
2016-05-19 16:36:11 +0800

    blear.core.ua

--- test/test.index.js
+++ test/test.index.js
@@ -7,56 +7,17 @@
 
 'use strict';
 
-var querystring = require('../src/index.js');
-
-var str = 'a=1&b=2&c=3&b=4';
-var buildURL = function (querystring) {
-    history.pushState(null, '', '?' + querystring);
-};
+var ua = require('../src/index.js');
 
 
 describe('测试文件', function () {
-    beforeAll(function () {
-        buildURL(str);
-    });
+    var uaList = [
+        "vivo_Y628/V1.0 Linux/3.10.28-svn930 Android/4.4.4 Release/09.22.2014 AppleWebKit/537.36 Profile/MIDP-2.0 Configuration/CLDC-1.1 Mobile Safari/537.36 System/Android 4.4.4"
+    ];
 
-    afterAll(function () {
-        buildURL('');
-    });
-
-    it('.toString', function () {
-        expect(querystring.toString()).toEqual(str);
-    });
-
-    it('.parse', function () {
-        expect(querystring.parse()).toEqual({
-            a: '1',
-            b: ['2', '4'],
-            c: '3'
-        });
-    });
-
-    it('.get', function () {
-        expect(querystring.get()).toEqual({
-            a: '1',
-            b: ['2', '4'],
-            c: '3'
-        });
-        expect(querystring.get('a')).toEqual('1');
-        expect(querystring.get('b')).toEqual(['2', '4']);
-        expect(querystring.get('c')).toEqual('3');
-        expect(querystring.get('d')).toEqual(undefined);
-    });
-
-    it('.set', function () {
-        buildURL(querystring.set('a', 5));
-        expect(querystring.get('a')).toEqual('5');
-    });
-
-    it('.remove', function () {
-        buildURL(querystring.remove('a'));
-        expect(querystring.get('a')).toEqual(undefined);
-        buildURL(querystring.remove('b'));
-        expect(querystring.get('b')).toEqual(undefined);
+    it('main', function () {
+        for(var i = 0; i < uaList.length; i++){
+            console.log(ua(uaList[i]));
+        }
     });
 });
